fc179fb8e6e1f454dc69c70825b9cefbbffebd67
Odilbekov O`lmasbek <oolmasbek754@gmail.com>
2026-04-04 14:38:33 +0500

    Add initial variable assignments and calculations

---
+++ app.js
@@ -0,0 +1,23 @@
+  let x = 10;
+
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+
+let x = 5;
+let y = 2;
+let z = x + y;
+
+let x = 5;
+let y = 2;
+let z = x * y;
+
+let a = 3;
+let x = (100 + 50) * a;
+
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
